4674006d2a55a6f1ba0ed13cd6e3f148371c6eae
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-05-09 23:57:20 +0200

    put Profile module into React Fragment tag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,21 @@
-// import { Fragment } from 'react';
+import { Fragment } from 'react';
 import { Profile } from 'components/Profile/Profile';
 import user from './Profile/user.json';
 
-
-
-export const App = () =>  (
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101',
-    //   }}
-    // >
-    //   React homework template
-    // </div>
+export const App = () => (
+  // <div
+  //   style={{
+  //     height: '100vh',
+  //     display: 'flex',
+  //     justifyContent: 'center',
+  //     alignItems: 'center',
+  //     fontSize: 40,
+  //     color: '#010101',
+  //   }}
+  // >
+  //   React homework template
+  // </div>
+  <Fragment>
     <Profile
       username={user.username}
       tag={user.tag}
@@ -24,4 +23,5 @@ export const App = () =>  (
       avatar={user.avatar}
       stats={user.stats}
     />
-  );
+  </Fragment>
+);
